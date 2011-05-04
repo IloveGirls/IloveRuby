6b090ee2577dc97ade4b5cad7af142e9779befd4
IloveGirls <s_pupkin@mail.ru>
2011-05-04 13:05:57 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '2c7e5a0d001abddd8f0ac4d648e20ccaced161842f3eca99881a6475dcf504cb6f8e115302e0cb3f5db0ee47e81edbd3ba14cb5f49e9fddd97e5d58767fb6a3b'
